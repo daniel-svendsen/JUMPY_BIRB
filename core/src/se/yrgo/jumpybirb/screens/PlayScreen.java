ba2e85d542f23d8de7ac8df1d5e08ce85a0b01db
AntonJava23 <antbri1009@skola.goteborg.se>
2024-03-05 12:28:43 +0100

    Adding some logic for score counting to obstacle

--- core/src/se/yrgo/jumpybirb/screens/PlayScreen.java
+++ core/src/se/yrgo/jumpybirb/screens/PlayScreen.java
@@ -23,7 +23,7 @@ public class PlayScreen implements Screen {
     public static final String TAG = PlayScreen.class.getSimpleName();
     private static final float TEXT_FONT_SCALE = 2.0f;
     private static final int OBSTACLE_COUNT = 4;
-    private static final float OBSTACLE_SPACING = 250f; // Spacing between tubes horizontally
+    private static final float OBSTACLE_SPACING = 300f; // Spacing between tubes horizontally
     private SpriteBatch batch;
     private Birb birb;
     private ScoreManager scoreManager;
