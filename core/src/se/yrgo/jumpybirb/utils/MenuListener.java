099dcac0b3fe5bc567e5578041c58caf3afd26e9
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-03-19 15:19:39 +0100

    added interface MenuListener

---
+++ core/src/se/yrgo/jumpybirb/utils/MenuListener.java
@@ -0,0 +1,7 @@
+package se.yrgo.jumpybirb.utils;
+
+public interface MenuListener {
+    void playButtonClicked();
+    void menuButtonClicked();
+    // Add more methods for other menu events as needed
+}
